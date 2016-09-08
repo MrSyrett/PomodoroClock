0f26bfecae16712a09019d6466780d6c282d846f
Michael <mrsyrett@gmail.com>
2016-09-08 18:45:11 +0000

    160908.1 built out basic funtionality

--- Project_Files/PomodoroClock.js
+++ Project_Files/PomodoroClock.js
@@ -1,24 +1,50 @@
 // JavaScript File
 $(document).ready(function(){
+    var Duration = parseInt($("#Duration").text());
+    var Break = parseInt($("#Break").text());
+    
+    $("#timeU1").on('click', function(){
+        Duration += 1;
+        $("#Duration").text(Duration);
+    })
+    $("#timeD1").on('click', function(){
+        Duration -= 1;
+        $("#Duration").text(Duration);
+    })
+    $("#breakU1").on('click', function(){
+        Break += 1;
+        $("#Break").text(Break);
+    })
+    $("#breakD1").on('click', function(){
+        Break -= 1;
+        $("#Break").text(Break);
+    })
+    
+    
+function startTimer(duration, display) {
+    
+    var timer = duration, minutes, seconds;
+    
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-window.onload = function () {
-    var display = document.querySelector('#clock'),
-        timer = new CountDownTimer(5),
-        timeObj = CountDownTimer.parse(5);
-
-    format(timeObj.minutes, timeObj.seconds);
-    
-    timer.onTick(format);
-    
-    $('#start').on('click', function () {
-        timer.start();
-    });
-    
-    function format(minutes, seconds) {
         minutes = minutes < 10 ? "0" + minutes : minutes;
         seconds = seconds < 10 ? "0" + seconds : seconds;
-        display.textContent = minutes + ':' + seconds;
-    }
-};
-    
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+
+$("#start").on('click', function () {
+    var clock = Duration * 60;
+        display = document.querySelector('#clock');
+    startTimer(clock, display);
+});
+
 })
