f6cc2bcb363f457058883dff06be1a4fef178c2b
Michael <mrsyrett@gmail.com>
2016-09-09 22:35:47 +0000

    160909.1 completed app functionality

--- Project_Files/PomodoroClock.js
+++ Project_Files/PomodoroClock.js
@@ -1,50 +1,119 @@
 // JavaScript File
 $(document).ready(function(){
-    var Duration = parseInt($("#Duration").text());
-    var Break = parseInt($("#Break").text());
+   
+ 
+    
+    var workingLength = parseInt($("#Duration").text());
+    var breakLength = parseInt($("#Break").text());
+    var inSession = true;
+    var timeRemaining = workingLength * 60;
+    var isPaused = false;
+   
+     function getFormatedTime(){
+      var m = Math.floor(timeRemaining / 60);
+      var s = (timeRemaining - m * 60);
+      
+      return (m < 10 ? '0' : '') + m + ':' + (s < 10 ? '0' : '') + s;
+    };
+
+        
+     function updateTime(){
+       $('#clock').text(getFormatedTime());   
+     }   
+
+     var timer = null;
+   
+    function pausePomodoro() {
+        isPaused = true;
+        clearInterval(timer);
+    }
+    
+    function nextStep() {
+      if (inSession === true) {
+        timeRemaining = breakLength * 60;
+        inSession = false;
+      } else {
+        inSession = true;
+        timeRemaining = workingLength * 60;
+    } startPomodoro();
+    }
+    
+    function startPomodoro() {
+      timer = setInterval(function () {
+        if (timeRemaining < 1) {
+            pausePomodoro();
+            nextStep();
+        } else
+          timeRemaining--;
+          updateTime();
+      }, 1000);
+    }
+    
+   
+    
+    
+    
+    // Button Controls
+    $("#reset").on('click', function(){
+    timeRemaining = workingLength * 60;
+      updateTime();    
+      inSession = !inSession;
+      if (!isPaused){
+        $("#pause").addClass('hidden');
+        $("#engage").removeClass('hidden');  
+        pausePomodoro();
+      }
+    });
+    $("#engage").on('click', function(){
+        $("#engage").addClass('hidden');
+        $("#pause").removeClass('hidden');
+        startPomodoro();
+    });
+       $("#pause").on('click', function(){
+         $("#pause").addClass('hidden');
+         $("#engage").removeClass('hidden');
+        pausePomodoro();
+    });
     
     $("#timeU1").on('click', function(){
-        Duration += 1;
-        $("#Duration").text(Duration);
-    })
-    $("#timeD1").on('click', function(){
-        Duration -= 1;
-        $("#Duration").text(Duration);
-    })
-    $("#breakU1").on('click', function(){
-        Break += 1;
-        $("#Break").text(Break);
-    })
-    $("#breakD1").on('click', function(){
-        Break -= 1;
-        $("#Break").text(Break);
-    })
-    
-    
-function startTimer(duration, display) {
-    
-    var timer = duration, minutes, seconds;
-    
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
+        if (worklength < 60){
+        workingLength += 1;
+        $("#Duration").text(workingLength);
+        timeRemaining = workingLength * 60;
+        updateTime();
+        } else {
+            return false;
         }
-    }, 1000);
-}
-
-
-$("#start").on('click', function () {
-    var clock = Duration * 60;
-        display = document.querySelector('#clock');
-    startTimer(clock, display);
-});
-
+    });
+    $("#timeD1").on('click', function(){
+        if (workingLength > 1){
+        workingLength -= 1;
+        $("#Duration").text(workingLength);
+        timeRemaining = workingLength * 60;
+        updateTime();
+        } else {
+            return false;
+        }
+    });
+    $("#breakU1").on('click', function(){
+        if (breakLength < 60){
+        breakLength += 1;
+        $("#Break").text(breakLength);
+        updateTime();
+        } else {
+            return false;
+        }
+    });
+    $("#breakD1").on('click', function(){
+        if (breakLength > 1){
+        breakLength -= 1;
+        $("#Break").text(breakLength);
+        updateTime();
+        } else {
+            return false;
+        }
+    });
+     
+    updateTime();  
+            
 })
